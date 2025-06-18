e5514a5f081503cdd832fbbc7e92b1649778129b
ALLEN5479 <811827671@qq.com>
2025-06-18 18:24:33 +0800

    my first commit

---
+++ src/main/java/com/example/ai_lesson/AiLessonApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLessonApplication.class, args);
+    }
+
+}
